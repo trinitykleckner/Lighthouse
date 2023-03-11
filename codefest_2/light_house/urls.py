e35eceb39d5c036914bcce95c3f588e35eb135fd
Trinity Kleckner <trinitykleckner@pop-os.localdomain>
2023-03-11 17:44:03 -0500

    added views for options and enpoint

--- codefest_2/light_house/urls.py
+++ codefest_2/light_house/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "light_house"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/options',views.options,name='options'),
+    path('/endpoint',views.endpoint,name='endpoint')
     # path('<int:id>/',views.detail, name="detail")
 ]
 
