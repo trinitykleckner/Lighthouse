61185b82d96da64cd29d57fe5e0efa8a3fd5aaf5
Trinity Kleckner <trinitykleckner@pop-os.localdomain>
2023-03-11 13:04:06 -0500

    added initial html template

--- base/views.py
+++ base/views.py
@@ -2,14 +2,19 @@ import gspread
 import json
 import requests
 
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
 
 
-# def index(request):                                                         / for testing 
-#     return HttpResponse("Hello, world. You're at the _base_ index.")        / for testing 
+def index(request): 
+    # return HttpResponse("Hello, world. You're at the _base_ index.")        / for testing 
+    template = loader.get_template('index.html')
+    # return HttpResponse(template.render(request))
+    return render(request,'index.html')
 
 def askGpt(task,content,language):
     input = {"task":task, "text":content, "language":language}
