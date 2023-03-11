221712e99a2647b5f5eacd32834a1963152f86d7
Trinity Kleckner <trinitykleckner@pop-os.localdomain>
2023-03-11 12:21:29 -0500

    started to add gpt function

--- base/views.py
+++ base/views.py
@@ -1,3 +1,7 @@
+import gspread
+import json
+import requests
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +10,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the _base_ index.")
+
+def askGpt(task,content,language):
+    input = {"task":task, "text":content, "language":language}
+    input = json.dumps(input)
+
+    url = "https://script.google.com/macros/s/AKfycbx_uPIrQMMoLMi4YOkIyU8nHGgcFUZIPO1Vkk6soo4a1AdVVl6Rzb-fJ7a7ifzTujc/exec"
+    response = requests.post(url, json=input)
+    print(repsonse)
